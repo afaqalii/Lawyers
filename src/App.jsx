98d5636fe15e55db57cb2d4bc34f8f449dfaf279
afaqalii <afaqali4244@gmai.com>
2023-02-07 20:40:03 -0800

    addded the files

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import lawyer from './assets/Lawyer.jpeg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={lawyer} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>I LOVE YOU ALLIYA</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          ALLIYA & AFAQ {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
