4a4adaccaae1957dcbd7511135aa61e4795e3276
afaqalii <afaqali4244@gmai.com>
2023-02-18 13:06:06 -0800

    Made the website responsive

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -74,21 +74,23 @@ const Home = () => {
         </div>
       </div>
 
-    {/*  */}
+    {/* 
+        LAWYERS IN NUMBER CONTAINER
+    */}
     <div className="lawyers_in_number section">
         <div className="lawyers_in_number_container container">
           <div className="lawyers_in_number_container_content">
             <h1>Lawyers. in numbers</h1>
             <div className="lawyers_in_number_container_subcontent">
-              <div className="lawyers_in_numer_container_subcontent_box">
+              <div className="lawyers_in_number_container_subcontent_box">
                 <h4>126</h4>
                 <p>Partners, lawyers, legal assistants, and articling students, all of whom come from top institutions worldwide.</p>
               </div>
-              <div className="lawyers_in_numer_container_subcontent_box">
+              <div className="lawyers_in_number_container_subcontent_box">
               <h4>6</h4>
                 <p>Domestic locations. We have offices in some of the country's largest cities, and a couple of small ones, too.</p>
               </div>
-              <div className="lawyers_in_numer_container_subcontent_box">
+              <div className="lawyers_in_number_container_subcontent_box">
               <h4>29</h4>
                 <p>Overseas partnerships. We have  international partners in several countries providing services in key areas. </p>
               </div>
